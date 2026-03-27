884d6c86f1b0b5f402d0a80b020f9820006f87cb
pol-dev-shinroo <manus.dream.language@gmail.com>
2026-03-27 19:12:04 +0900

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  // We removed 'output: "export"' so Next.js knows this is a full-stack app!
   // Note: If you are using next/image, you might need to set unoptimized: true
   // images: { unoptimized: true }
 };
